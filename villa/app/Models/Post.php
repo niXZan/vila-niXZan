c67dcaf96fa4600a1d42257dc5128e691e04d611
Thunder <151688223+Mathiack@users.noreply.github.com>
2025-03-06 21:51:51 -0300

    add: mais comandos

--- villa/app/Models/Post.php
+++ villa/app/Models/Post.php
@@ -9,4 +9,10 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'username',
+        'content',
+        'image',
+    ];
 }
