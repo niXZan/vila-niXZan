419e69ea8113773e3f0c3c0c9685a83e75bc36d3
Thunder <151688223+Mathiack@users.noreply.github.com>
2025-03-13 21:19:44 -0300

    a

--- villa/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/posts', [\App\Http\Controllers\PostController::class,'index']);
