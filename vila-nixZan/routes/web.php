eaa94c395d0879e3ca502340f389853c42893b0b
Wesw3s <151688352+Wesw3s@users.noreply.github.com>
2025-03-13 21:37:18 -0300

    route feito

--- vila-nixZan/routes/web.php
+++ vila-nixZan/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
+
+Route::get('/post', [\App\Http\Controllers\PostController::class, 'index']);
+
+Route::get('/post/{id}', [\App\Http\Controllers\PostController::class, 'show']);
